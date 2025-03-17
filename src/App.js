e917ae5596b339985d83b0e6838bd0148b7988ff
SnehanjaliBiswal <biswalsnehanjali77@gmail.com>
2025-03-17 12:51:44 +0530

    jj

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./Auth/SignIn";
+import Forgot from "./Auth/Forgot";
+import LayoutDashboard from "./Layout/LayoutDashboard";
+import LayoutPatients from "./Layout/LayoutPatients";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/sign-in" element={<SignIn />}/>
+        <Route path="/" element={<LayoutDashboard/>}/>
+        <Route path="/forgot-password" element={<Forgot />}/>
+        <Route path= "/patients" element={<LayoutPatients/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
